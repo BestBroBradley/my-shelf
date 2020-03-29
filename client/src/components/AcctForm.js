910bfac1d08440fb201cce4a79ab5dc9b8442b88
Allison Bradley <allison.bradley@outlook.com>
2020-03-29 13:56:07 -0600

    Worked on controlled input for newacct

--- client/src/components/AcctForm.js
+++ client/src/components/AcctForm.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import { Form, Input, Button } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Form, Input, Button, Message } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import API from '../utils/API'
 
 export const AcctForm = () => {
 
+    const [formState, updateFormState] = useState({
+        username: '',
+        password: '',
+        email: '',
+        usernameError: true,
+        passwordError: true,
+        emailError: true
+    })
 
+    const handleUpdate = (event) => {
+        const {name, value} = event.target
+        updateFormState({
+            ...formState,
+            [name]: value
+        })
+    }
     const handleSubmit = (event) => {
         event.preventDefault()
         const username = document.getElementById('username').value
@@ -30,21 +45,42 @@ export const AcctForm = () => {
                         id='username'
                         control={Input}
                         label='Username'
+                        name='username'
                         placeholder='Username'
+                        value={formState.username}
+                        onChange={handleUpdate}
+                        error={{
+                            content: 'Username must be at least 8 characters',
+                            pointing: 'below',
+                          }}
                     />
                     <Form.Field
                         id='password'
                         control={Input}
                         label='Password'
+                        name='password'
                         type='password'
                         placeholder='Password'
+                        value={formState.password}
+                        onChange={handleUpdate}
+                        error={{
+                            content: 'Password must contain at least 1 uppercase letter, 1 lowercase letter, and a number',
+                            pointing: 'below',
+                          }}
                     />
                 </Form.Group>
                 <Form.Field
                     id='email'
                     control={Input}
                     label='Email'
+                    name='email'
                     placeholder='joe@schmoe.com'
+                    value={formState.email}
+                    onChange={handleUpdate}
+                    error={{
+                        content: 'Please enter a valid email address',
+                        pointing: 'below',
+                      }}
                 />
                 <Form.Field
                     id='form-button-control-public'
