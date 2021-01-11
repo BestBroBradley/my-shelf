11ed6c2e5b20c78b242031b397cb5e918cd2ac91
Allison Bradley <allison.bradley@outlook.com>
2021-01-11 11:39:25 -0700

    Updated mongoose connection

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/myshelf");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/myshelf", { useNewUrlParser: true });
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
